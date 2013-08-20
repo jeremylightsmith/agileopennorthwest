e15c10090d35085bd0079bc987a845fc4c206796
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2013-08-19 17:46:44 -0700

    adding and styling a contact form that adds to contact

--- 2013/reg_panels.php
+++ 2013/reg_panels.php
@@ -35,8 +35,44 @@
 <?php } elseif ( $event_status == 'past' ) { ?>
 
   <p id="title">Conference Complete!</p>
-  <p>Agile Open Northwest <?php echo $event_year; ?> was a success. You can read session notes <a href="http://aonw2013.posterous.com">here</a>. We hope to see you at
-   the next Agile Open Northwest event.</p>
+  <p>Agile Open Northwest <?php echo $event_year; ?> was a success. You can read session notes 
+    <a href="http://aonw2013.posterous.com">here</a>. We hope to see you at
+    the next Agile Open Northwest event.</p>
+
+  <p id="title">Signup as an AONW Friend</p>
+  <p>Be the first to hear about next year's conference!</p>
+
+  <script type="text/javascript">
+  $(function() {
+    $("button.let-me-know").click(function() {
+      $(this).hide().next().show();
+      return false;
+    })
+  })
+  </script>
+  <button class="let-me-know">Let me know about 2014!</button>
+
+  <form class="let-me-know" style="display:none; text-align: left;" method="post" action="http://app.icontact.com/icp/signup.php" name="icpsignup" id="icpsignup3663" accept-charset="UTF-8" onsubmit="return verifyRequired3663();">
+    <input type="hidden" name="redirect" value="http://www.icontact.com/www/signup/thanks.html">
+    <input type="hidden" name="errorredirect" value="http://www.icontact.com/www/signup/error.html">
+
+    <p><span class="required">*</span>Email<br/><input type="text" name="fields_email"></p>
+    <p><span class="required">*</span>First Name<br/><input type="text" name="fields_fname"></p>
+    <p><span class="required">*</span>Last Name<br/><input type="text" name="fields_lname"></p>
+    <p>Business<br/><input type="text" name="fields_business"></p>
+    <p>City<br/><input type="text" name="fields_city"></p>
+    <input type="hidden" name="listid" value="44965">
+    <input type="hidden" name="specialid:44965" value="MWJL">
+
+    <input type="hidden" name="clientid" value="218221">
+    <input type="hidden" name="formid" value="3663">
+    <input type="hidden" name="reallistid" value="1">
+    <input type="hidden" name="doubleopt" value="0">
+
+    <p><span class="required">*</span> = Required Field</p>
+
+    <p><input type="submit" name="Submit" value="Submit"></p>
+  </form>
 
 <?php } ?>
 
