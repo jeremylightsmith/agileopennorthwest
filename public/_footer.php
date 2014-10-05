b585a54dedac923f3bd060845b6f88e9640c9efb
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2014-10-04 18:33:26 -0700

    added space on the bottom of the page, and made side nav fit better

--- public/_footer.php
+++ public/_footer.php
@@ -1,6 +1,18 @@
     </div>
   </div>
 </div>
+
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+      </div>
+      <div class="col-md-9 footer-content">
+        &copy; Agile Open Northwest 2014
+      </div>
+    </div>
+  </div>
+</footer>
     
 <script src="/js/aonw.js"></script>
 <script src="//localhost:35729/livereload.js"></script>
